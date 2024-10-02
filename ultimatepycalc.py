1d62a6e26b276290fc0df9a4e58e70e166a1cdee
goober-183 <151787624+goober-183@users.noreply.github.com>
2024-10-02 17:14:04 -0500

    merged files

--- ultimatepycalc.py
+++ ultimatepycalc.py
@@ -6,4 +6,18 @@ def multiply():
     y = int(input())
     print ("The result is " + str (x*y))
 
+def greaterthan():
+    #im just going to have to write a system that lets you choose between the 2, just too lazy to do that rn
+    print ("greater than Program by team-res")
+    print ("Give me an x")
+    x = int(input())
+    print ("Give me a y")
+    y = int(input())
+    if x > y:
+        print (str(x) + " is greater than " + str(y))
+    elif y > x:
+        print (str(y) + " is greater than " + str(x))
+    else:
+        print ("Both numbers are equal")
+
 multiply()
