7fcd1786eaad0c296bf22aa8f05980de2783e844
Ricky <183110334+rch1431@users.noreply.github.com>
2024-10-02 14:26:29 -0500

    Add files via upload

---
+++ greaterthan.py
@@ -0,0 +1,11 @@
+print ("greater than Program by team-res")
+print ("Give me an x")
+x = int(input())
+print ("Give me a y")
+y = int(input())
+if x > y:
+    print (str(x) + " is greater than " + str(y))
+elif y > x:
+    print (str(y) + " is greater than " + str(x))
+else:
+    print ("Both numbers are equal")
