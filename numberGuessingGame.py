0588f0265fee250b6b6f538aa7059d8ceb60a432
goober-183 <151787624+goober-183@users.noreply.github.com>
2024-10-05 14:32:54 -0500

    added numberGuessingGame.py
    the first print statement is political :D

---
+++ numberGuessingGame.py
@@ -0,0 +1,7 @@
+print ("Vim Is Better than emacs, all i want is a text editor, Not a whole operating system")
+print ("Guess my number")
+x = input()
+if x == str(5):
+   print ("you guessed my number!")
+else:
+    print ("wrong :(")
