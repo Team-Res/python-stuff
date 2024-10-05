fcaf812799b1616e66ed33f5d453b9a5ee56c38b
goober-183 <151787624+goober-183@users.noreply.github.com>
2024-10-05 15:30:58 -0500

    Added Comments

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,6 +1,9 @@
 print ("Vim Is Better than emacs, all i want is a text editor, Not a whole operating system")
 print ("Guess my number")
+#gets user input
 x = input()
+#if the number the user inputed is = 5, give positive message
+#If not, Then give negative message
 if x == str(5):
    print ("you guessed my number!")
 else:
